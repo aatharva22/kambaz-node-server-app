e39dbaba88647583048b93570614c413879fea5d
aatharva22 <atharvapu10@gmail.com>
2025-12-05 15:49:51 -0500

    added comment to domain

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
+    //domain: process.env.SERVER_URL,
   };
 }// Adjust session settings for production environment
 app.use(session(sessionOptions));
